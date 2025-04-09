7e77b59f382be8a36eb754a66e1bdb90f1c2ce86
applyinnovations <alexander@applyinnovations.com.au>
2025-04-09 15:03:05 +1000

    updated extension config for minimal wasm binary

--- extension_config_wasm.cmake
+++ extension_config_wasm.cmake
@@ -3,12 +3,12 @@
 ################################################################################
 #
 
-duckdb_extension_load(json DONT_LINK)
+# duckdb_extension_load(json DONT_LINK)
 duckdb_extension_load(parquet DONT_LINK)
-duckdb_extension_load(autocomplete DONT_LINK)
+# duckdb_extension_load(autocomplete DONT_LINK)
 
-duckdb_extension_load(icu DONT_LINK)
-duckdb_extension_load(tpcds DONT_LINK)
-duckdb_extension_load(tpch DONT_LINK)
+# duckdb_extension_load(icu DONT_LINK)
+# duckdb_extension_load(tpcds DONT_LINK)
+# duckdb_extension_load(tpch DONT_LINK)
 
 #duckdb_extension_load(httpfs DONT_LINK)
